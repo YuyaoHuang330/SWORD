8905533a3aaaf6215b2371b563f80c5094657173
Tosykie <nw2y47@outlook.com>
2026-04-27 01:51:01 +0800

    Init tidy lib

---
+++ swordlib/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2026, Kedar Hippalgaonkar's Materials by Design Lab
+# Distributed under the terms of the MIT License.
+
+"""
+SWORDlib is a Python library for SWORD - Symmetry and Wyckoff-sequence of Ordered and Disordered crystals.
+"""
+
+__copyright__  = "Copyright 2026, Kedar Hippalgaonkar's Materials by Design Lab"
+__version__    =  "0.1.dev1"
+__author__     = "Yuyao Huang, Wei Nong, Shuya Yamazaki, Martin Hoffmann Petersen, Jianghai Wang, Ruiming Zhu, Kedar Hippalgaonkar"
+__maintainer__ = "Yuyao Huang"
+__email__      = "YUYAO003@e.ntu.edu.sg"
+__status__     = "Development"
+__date__       = "April 27, 2026"
